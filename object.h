933509a227a67cbdb05def08acb2d420d8eef968
Jingbin Zhang <451404439@qq.com>
2018-03-06 22:20:02 +0800

    resource manager

--- object.h
+++ object.h
@@ -8,6 +8,7 @@
 #define OBJECT_H_
 
 #include <string>
+#include <limits.h>
 
 namespace ps{
 
@@ -38,12 +39,45 @@ namespace ps{
 
 		std::vector<std::string> funcs;
 
-		unorder_map<std::string, std::vector<std::string>> inputs;
+		unorder_map<std::string, std::vector<std::string>> inputs; // func->inputs
 
-		unorder_map<std::string, std::vector<std::string>> outputs; 
+		unorder_map<std::string, std::vector<std::string>> outputs; //func->outputs
 
-		unorder_map<std::string, int> qos;
+		unorder_map<std::string, int> qos; //func->qos
 
+		unorder_map<std::string, std::vector<std::string>> op_funcs_;//output->func 
+
+		void AddFunc(std::string func, std::vector<std::string> ips, 
+			std::vector<std::string> ops, int value) {
+			funcs.push_back(func);
+			inputs[func] = ips;
+			outputs[func] = ops;
+			qos = value;
+
+			for (std::string op : ops){
+				op_funcs_[op].push_back(func);
+			}
+  		}
+  		/*返回func以及其qos*/
+  		std::pair<std::string, int> FindFunc(std::string op) {
+  			int qostemp;
+  			std::pair<std::string, int> result;
+  			if (!op_funcs_[op].empty()) {
+	  			qostemp = INT_MAX; 
+	  			for (int i=0; i<op_funcs_[op].size(); i++){
+	  				std::string func = op_funcs_[op][i];
+	  				if (qos[func] < qostemp){
+	  					result = std::make_pair(func, qos[func]);
+	  					qostemp = qos[func];
+	  				}
+	  			}
+  			}
+  			return result;
+  		} 
+
+  		std::vector<std::string> GetOutputs() {
+  			
+  		}
 	};
 
 
--- resource.h
+++ resource.h
@@ -8,14 +8,45 @@
 #define RESOURCE_H_
 
 #include <string>
+#include "object.h"
 
 namespace ps{
 
+	/*worker对应的资源管理器，管理所有接入的objects*/
 	class ResManager{
 	public:
-		
+		void AddObject(Object* obj){
+			objects_[obj->uid] = obj;
+			for (auto it : op_funcs){
+				op_objs_[it.first]=obj;
+			}
+		}
+
+		void AddFunc(int id, std::string func, std::vector<std::string> ips, 
+			std::vector<std::string> ops, int value) {
+			objects_[id]->AddFunc(func, ips, ops, value);
+  		}
+  		/*返回object和func*/
+  		std::pair<int, std::string> FindFunc(std::string op) {
+  			int qostemp;
+  			std::pair<int, std::string> result;
+  			if (!op_objs_[op].empty()) {
+	  			qostemp = INT_MAX; 
+	  			for (int i=0; i<op_objs_[op].size(); i++){
+	  				int id = op_objs_[op][i];
+	  				std::pair<std::string, int> obj_qos_ = objects_[id]->FindFunc(op);
+	  				if (obj_qos_.second < qostemp){
+	  					result = std::make_pair(id, obj_qos_.first);
+	  					qostemp = obj_qos_.second;
+	  				}
+	  			}
+  			}
+  			return result;
+  		} 
+
 	private:
-		
+		std::unordered_map<int, Object*> objects_;
+		std::unordered_map<std::string, std::vector<int>> op_objs_; //output->object
 	};
 
 
