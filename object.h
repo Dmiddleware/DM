fec89e9e193f8d01bf919d61b2d1d80ea5f9f7ab
PKU_YanKun <786235337@qq.com>
2018-03-23 21:09:57 +0800

    fix bugs

--- object.h
+++ object.h
@@ -1,84 +1,115 @@
 /*
  * object.h
  *
- *  Created on: 2018年3月6日
- *      Author: zjbpoping
  */
 #ifndef OBJECT_H_
 #define OBJECT_H_
 
 #include <string>
 #include <limits.h>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
 
-namespace ps{
+namespace ps {
 
-	/*QoS属性指标*/
-	// enum Attribute {
-	//   RESPONSE_TIME, THROUGHPUT, RELIABILITY, STABILITY, TRUST, OTHER
-	// };
+/*QoS属性指标*/
+// enum Attribute {
+//   RESPONSE_TIME, THROUGHPUT, RELIABILITY, STABILITY, TRUST, OTHER
+// };
 
-	struct Object{
+struct Object {
 
-		Object(int id) {
-			uid = id;
-		} 
+	Object(int id) {
+		uid = id;
+	}
 
-		enum DeviceType { FIXED, MOVABLE };
+	enum DeviceType { FIXED, MOVABLE };
+	//the id of the object
+	int uid;
+	//the ip address of the object
+	std::string uri;
+	//the port of the object
+	int port;
+	//correspond to the above mentioned "enum DeviceType"
+	DeviceType type;
+	//the location of the object
+	float loc_x;
+	float loc_y;
 
-		int uid;
+	//the id of worker connected to this object
+	int worker_id;
 
-		std::string uri;
+	std::vector<std::string> funcs;
+	//the smaller the better
+	std::unordered_map<std::string, int> qos; //func->qos
+	std::unordered_map<std::string, std::vector<std::string>> op_funcs_;//output->func
+	//the inputs and outputs corresponding to function
+	std::unordered_map<std::string, std::vector<std::string>> inputs;
+	std::unordered_map<std::string, std::vector<std::string>> outputs;
 
-		int port;
-
-		DeviceType type;
-
-		float loc_x;
-
-		float loc_y;
-
-		std::vector<std::string> funcs;
-
-		unorder_map<std::string, std::vector<std::string>> inputs; // func->inputs
-
-		unorder_map<std::string, std::vector<std::string>> outputs; //func->outputs
-
-		unorder_map<std::string, int> qos; //func->qos
-
-		unorder_map<std::string, std::vector<std::string>> op_funcs_;//output->func 
-
-		void AddFunc(std::string func, std::vector<std::string> ips, 
-			std::vector<std::string> ops, int value) {
+	/* add a function of this object */
+	void AddFunc(std::string func,  int value) {
+		bool judge = FindFunc(func);
+		if (judge) {
 			funcs.push_back(func);
-			inputs[func] = ips;
-			outputs[func] = ops;
-			qos = value;
-
-			for (std::string op : ops){
-				op_funcs_[op].push_back(func);
+			qos[func] = value;
+		} else {
+			qos[func] = std::min(qos[func], value);
+		}
+	}
+	/* find the best function(the lowest qos) for the option*/
+	std::pair<std::string, int> FindFuncByop(std::string op) {
+		int qostemp;
+		std::pair<std::string, int> result;
+		if (!op_funcs_[op].empty()) {
+			qostemp = INT_MAX;
+			int op_funcs_po_size = op_funcs_[op].size();
+			for (int i = 0; i < op_funcs_po_size; i++) {
+				std::string func = op_funcs_[op][i];
+				if (qos[func] < qostemp) {
+					result = std::make_pair(func, qos[func]);
+					qostemp = qos[func];
+				}
 			}
-  		}
-  		/*返回func以及其qos*/
-  		std::pair<std::string, int> FindFunc(std::string op) {
-  			int qostemp;
-  			std::pair<std::string, int> result;
-  			if (!op_funcs_[op].empty()) {
-	  			qostemp = INT_MAX; 
-	  			for (int i=0; i<op_funcs_[op].size(); i++){
-	  				std::string func = op_funcs_[op][i];
-	  				if (qos[func] < qostemp){
-	  					result = std::make_pair(func, qos[func]);
-	  					qostemp = qos[func];
-	  				}
-	  			}
-  			}
-  			return result;
-  		} 
+		}
+		return result;
+	}
+	/*look for the function */
+	bool FindFunc(std::string func) {
+		bool judge = 1;
+		int funcs_size = funcs.size();
+		for(int i = 0; i < funcs_size; i++){
+			if(funcs[i] == func){
+				judge = 0; 
+				break;
+			}
+		}
+		return judge;
+	}
+	/*
+	void AddFunc(std::string func, std::vector<std::string> ips,
+		std::vector<std::string> ops, int value) {
 
-  		std::vector<std::string> GetOutputs() {
-  			
-  		}
-	};
 
+		funcs.push_back(func);
+		inputs[func] = ips;
+		outputs[func] = ops;
+		qos = value; // qos[func] = value
+
+		for (std::string op : ops){
+			op_funcs_[op].push_back(func);
+		}*/
+
+	/*返回func以及其qos*/
+	/*
+	std::vector<std::string> GetOutputs() {
+
+	}
+	*/
+};
 
 }
+
+#endif /*OBJECT_H_*/
